8721fa5ac2e36b6eaabcdb270ab2029657a1024f
David Petringa <david@ajaxtransport.com>
2017-11-07 12:54:31 -0500

    added header to email

--- index.php
+++ index.php
@@ -4,7 +4,13 @@ Ball Cactus'>
 
 <p>A change</p>
 
-<p>Checking to see if can fax from Digital Ocean</p>
+<p>Checking to see if able to fax from Digital Ocean</p>
 <?php
-    mail('17062536984@efaxsend.com', 'hello', 'sue, How are you?');
+	$to = "17062536984@efaxsend.com";
+    $subject = "Hello";
+    $txt = "Sue, How are you?";
+    $headers = "From: fax@ajaxtransport.com" . "\r\n" .
+    "CC: david@ajaxtransport.com";
+
+    mail($to,$subject,$txt,$headers);
 ?>
