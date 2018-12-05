ee0a547b2c5b4a8542b4dbf029bc8e6011e69117
David Petringa <david@ajaxtransport.com>
2018-12-05 11:53:42 -0500

    added stat counter

--- index.php
+++ index.php
@@ -5,12 +5,21 @@ Ball Cactus'>
 <p>A change</p>
 
 <p>Checking to see if able to fax from Digital Ocean</p>
-<?php
-	$to = "17062536984@efaxsend.com";
-    $subject = "Hello";
-    $txt = "Sue, How are you?";
-    $headers = "From: fax@ajaxtransport.com" . "\r\n" .
-    "CC: david@ajaxtransport.com";
 
-    mail($to,$subject,$txt,$headers);
-?>
+
+<!-- Default Statcounter code for Harvard Extension DWA
+http://http//www.dukesnuz.com -->
+<script type="text/javascript">
+var sc_project=11889370;
+var sc_invisible=1;
+var sc_security="6ab59e53";
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="//c.statcounter.com/11889370/0/6ab59e53/1/" alt="Web
+Analytics"></a></div></noscript>
+<!-- End of Statcounter Code -->
